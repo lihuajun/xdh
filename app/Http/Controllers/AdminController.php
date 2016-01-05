575ba409624d69a58da7c674910c40ff6b87addd
lihuajun <449157442@qq.com>
2016-01-05 14:08:44 +0000

    init

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lihuajun
+ * Date: 16-1-5
+ * Time: 下午1:53
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+
+class AdminController extends BaseController
+{
+    use DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    
+
+}
