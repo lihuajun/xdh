fe582c69a0e29e5af0486459284e3fd6835ef01a
lihuajun <449157442@qq.com>
2016-01-05 17:43:29 +0000

    新加入文件

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -28,6 +28,6 @@ class AdminController extends BaseController
 
     }
 
-    
+
 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Admin/index');
 });
+
+
+//Route::any('/test',function(){
+//
+//});
